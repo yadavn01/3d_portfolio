4e15f7adad0026728c2d8b5773647159e13fb028
yadavn01 <ynaman29@gmail.com>
2023-07-09 23:09:39 +1200

    navbar wip

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, {useEffect, useState} from 'react'
 import { Link } from 'react-router-dom';
 import {styles} from '../styles';
@@ -6,9 +7,20 @@ import {logo, menu, close} from '../assets'
 
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
-    <nav className={'${styles.paddingX}'}></nav>
+    <nav className={'${styles.paddingX} w-full flex items-centre py-5 fixed top-0 z-20 bg-primary'}>
+      <div className='w-full flex justify=between items-center max-w-7x1 mx-auto'>
+        <Link to="/" className='flex items-center gap-2' onClick={() => {
+          setActive("");
+          window.scrollTo(0,0);
+        }}> 
+        <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
+        <p className='text-white text-[18px] font-bold cursor-pointer'> Naman <span className='sm:block hidden'>Web Developer</span></p>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar  
